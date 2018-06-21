574edd428a304bc37eb4080d2c64f83b20376162
vida.opperman <vida.opperman@dariel.co.za>
2018-06-21 15:16:38 +0200

    Initial DutchTreat commit

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -20,14 +20,14 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("<html><body><h1>Salut Toi</h1></body></html>");
             });
         }
     }
